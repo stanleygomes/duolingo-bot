7e4e925418e024912d36ca7128da0b79effabf39
stanley <stanleygomesdasilva@gmail.com>
2020-02-01 23:13:24 -0300

    all working

--- src/api/crawler/crawlerParamService.js
+++ src/api/crawler/crawlerParamService.js
@@ -13,8 +13,62 @@ const crawlerParam = {
     selectedStory: '#react-root > div > div > div > div > div.whole-page.desktop > div.app-container > div > div > div > div:nth-child(2) > a:nth-child(4)',
     buttonStartStory: '.story-starter-start-story',
     buttonContinueStoryDisabled: '.play-controls button.continue:disabled',
-    buttonContinueStoryEnabled: '.play-controls button.continue:not([disabled])'
-  }
+    buttonContinueStoryEnabled: '.play-controls button.continue:not([disabled])',
+    multipleChoiseChallenge: '.multiple-choice-challenge',
+    multipleChoiseChallengeList: '.multiple-choice-challenge .challenge-answers li',
+    arrangeChallenge: '.arrange-challenge',
+    arrangeChallengeList: '.arrange-challenge .phrase-bank .phrase',
+    tapChallenge: '.tap-challenge',
+    tapChallengeList: '.tap-challenge .tappable-phrase',
+    matchChallenge: '.match-challenge',
+    matchChallengeList: '.match-challenge .tokens li'
+  },
+  answers: {
+    first: '… o que Julia quer comer.',
+    second: 'Ele também é vegetariano.',
+    third: 'Sim, isso mesmo.',
+    fourth: '… mentiu sobre seu nome porque ele gostou da Julia.'
+  },
+  phraseSequences: {
+    first: ['Where', 'are', 'you', 'from']
+  },
+  singleTapAnswer: {
+    first: 'pets'
+  },
+  matchChallengeDatabase: [
+    {
+      en: 'Where',
+      br: 'onde'
+    },
+    {
+      en: 'But',
+      br: 'mas'
+    },
+    {
+      en: 'Yes',
+      br: 'sim'
+    },
+    {
+      en: 'you',
+      br: 'você'
+    },
+    {
+      en: 'is',
+      br: 'é'
+    },
+    {
+      en: 'No',
+      br: 'não'
+    },
+    {
+      en: 'want',
+      br: 'quer'
+    },
+    {
+      en: 'salad',
+      br: 'salada'
+    }
+  ]
 }
 
 module.exports = crawlerParam
--- src/api/crawler/crawlerService.js
+++ src/api/crawler/crawlerService.js
@@ -11,7 +11,7 @@ const execute = (req, res) => {
     step3
       .end()
       .then((res) => {
-        console.log(res)
+        console.log('Execução finalizada com sucesso!')
         resolve(res)
       })
       .catch(error => {
@@ -33,29 +33,158 @@ const login = (nightmareStack) => {
 
 const openStory = (nightmareStack) => {
   return nightmareStack
-    .wait(crawlerParams.defaultTimeout)
+    .wait(crawlerParams.elements.storiesHeader)
     .click(crawlerParams.elements.storiesHeader)
-    .wait(crawlerParams.defaultTimeout)
+    .wait(crawlerParams.elements.selectedStory)
     .click(crawlerParams.elements.selectedStory)
-    .wait(crawlerParams.defaultTimeout)
+    .wait(crawlerParams.elements.buttonStartStory)
     .wait(crawlerParams.elements.buttonStartStory)
     .click(crawlerParams.elements.buttonStartStory)
-    // .wait(crawlerParams.elements.logoHeader, 30000)
-    // .evaluate(() => document.querySelector('#r1-0 a.result__a').href)
 }
 
 const playStory = (nightmareStack) => {
-  const clickTimes1 = 5
+  nightmareStack = clickContinue(nightmareStack, 5)
 
-  for (let i = 0; i < clickTimes1; i++) {
+  nightmareStack
+    .wait(crawlerParams.elements.multipleChoiseChallenge)
+    .evaluate(multipleChoiseChallenge, crawlerParams.answers.first, crawlerParams)
+
+  nightmareStack = clickContinue(nightmareStack, 6)
+
+  nightmareStack
+    .wait(crawlerParams.elements.multipleChoiseChallenge)
+    .evaluate(multipleChoiseChallenge, crawlerParams.answers.second, crawlerParams)
+
+  nightmareStack = clickContinue(nightmareStack, 2)
+
+  nightmareStack
+    .wait(crawlerParams.elements.arrangeChallenge)
+    .evaluate(arrangeChallenge, crawlerParams.phraseSequences.first, crawlerParams)
+
+  nightmareStack = clickContinue(nightmareStack, 6)
+
+  nightmareStack
+    .wait(crawlerParams.elements.tapChallenge)
+    .evaluate(tapChallenge, crawlerParams.singleTapAnswer.first, crawlerParams)
+
+  nightmareStack = clickContinue(nightmareStack, 7)
+
+  nightmareStack
+    .wait(crawlerParams.elements.multipleChoiseChallenge)
+    .evaluate(multipleChoiseChallenge, crawlerParams.answers.third, crawlerParams)
+
+  nightmareStack = clickContinue(nightmareStack, 4)
+
+  nightmareStack
+    .wait(crawlerParams.elements.multipleChoiseChallenge)
+    .evaluate(multipleChoiseChallenge, crawlerParams.answers.fourth, crawlerParams)
+
+  nightmareStack = clickContinue(nightmareStack, 1)
+
+  nightmareStack
+    .wait(crawlerParams.elements.matchChallenge)
+    .evaluate(matchChallenge, crawlerParams.matchChallengeDatabase, crawlerParams)
+
+  nightmareStack = clickContinue(nightmareStack, 3)
+
+  nightmareStack.wait(10000)
+
+  return nightmareStack
+}
+
+const clickContinue = (nightmareStack, times) => {
+  for (let i = 0; i < times; i++) {
     nightmareStack
       .wait(crawlerParams.elements.buttonContinueStoryEnabled)
       .click(crawlerParams.elements.buttonContinueStoryEnabled)
   }
 
-  nightmare.wait(60000)
-
   return nightmareStack
+}
+
+const arrangeChallenge = (sequence, crawlerParams) => {
+  const wordsHTML = document.querySelectorAll(crawlerParams.elements.arrangeChallengeList)
+
+  for (let i = 0; i < sequence.length; i++) {
+    for (let j = 0; j < wordsHTML.length; j++) {
+      const element = wordsHTML[j]
+      const word = element.textContent
+
+      if (word === sequence[i]) {
+        element.click()
+      }
+    }
+  }
+}
+
+const tapChallenge = (answer, crawlerParams) => {
+  const wordsHTML = document.querySelectorAll(crawlerParams.elements.tapChallengeList)
+
+  for (let j = 0; j < wordsHTML.length; j++) {
+    const element = wordsHTML[j]
+    const word = element.textContent
+
+    if (word === answer) {
+      element.click()
+    }
+  }
+}
+
+const matchChallenge = (database, crawlerParams) => {
+  const tokensHTML = document.querySelectorAll(crawlerParams.elements.matchChallengeList)
+
+  for (let i = 0; i < tokensHTML.length; i++) {
+    const element = tokensHTML[i]
+    const token = element.textContent
+    let databaseCompare = null
+
+    for (let j = 0; j < database.length; j++) {
+      const itemDatabase = database[j]
+
+      if (itemDatabase.en === token) {
+        databaseCompare = itemDatabase.br
+        element.childNodes[0].click()
+      }
+    }
+
+    if (databaseCompare !== null) {
+      for (let j = 0; j < tokensHTML.length; j++) {
+        const elementCompare = tokensHTML[j]
+        const tokenCompare = elementCompare.textContent
+
+        if (databaseCompare === tokenCompare) {
+          elementCompare.childNodes[0].click()
+        }
+      }
+    } else {
+      for (let j = 0; j < tokensHTML.length; j++) {
+        const elementCompare = tokensHTML[j]
+
+        element.childNodes[0].click()
+        elementCompare.childNodes[0].click()
+      }
+    }
+  }
+}
+
+const multipleChoiseChallenge = (rightAnswer, crawlerParams) => {
+  const answersHTML = document.querySelectorAll(crawlerParams.elements.multipleChoiseChallengeList)
+
+  for (let i = 0; i < answersHTML.length; i++) {
+    const element = answersHTML[i]
+    const phrase = element.textContent
+    if (phrase === rightAnswer) {
+      // button
+      if (element.childNodes && element.childNodes[0]) {
+        element.childNodes[0].click()
+        return true
+      } else {
+        throw new Error('Não encontrei o botão para clicar!')
+      }
+    }
+  }
+
+  throw new Error('Não encontrei a resposta!')
 }
 
 module.exports = {
